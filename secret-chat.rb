3d8917ec9367011737316a44a245106d6a51315b
unknown <SONY@.(none)>
2011-08-13 23:58:08 -0500

    fixing method send message

--- secret-chat.rb
+++ secret-chat.rb
@@ -4,7 +4,7 @@ require 'redis'
 
 get '/send_message' do
   "Hello from Sinatra on Heroku!"
-  puts "REDIS EVIROMENT #{ENV["REDISTOGO_URL"]}"
+  puts "REDIS EVIROMENT #{ENV.inspect}"
 #  uri = URI.parse(ENV["REDISTOGO_URL"] | "http://localhost:6379")
 #  puts "URI #{uri.inspect}"
 #  redis_client = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
