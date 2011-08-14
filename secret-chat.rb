06f6c936298ab0036458445981f90eb1f59ba316
unknown <SONY@.(none)>
2011-08-13 23:38:58 -0500

    starting redis

--- secret-chat.rb
+++ secret-chat.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'sinatra'
+require 'redis'
 
-get '/' do
+get '/send_message' do
   "Hello from Sinatra on Heroku!"
+  puts "REDIS EVIROMENT #{ENV["REDISTOGO_URL"]}"
+#  uri = URI.parse(ENV["REDISTOGO_URL"] | "http://localhost:6379")
+#  puts "URI #{uri.inspect}"
+#  redis_client = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+#  puts "REDIS CLIENT :#{ENV["REDISTOGO_URL"]}"
+#  puts redis_client.inspect
 end
