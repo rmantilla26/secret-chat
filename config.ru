fe8e563d943d89cf1911b7871ad93d69e6a9e3b0
unknown <SONY@.(none)>
2011-08-15 20:39:55 -0500

    estable 0.0.1

--- config.ru
+++ config.ru
@@ -1,11 +1,5 @@
 require './secret-chat'
 require 'sinatra'
-
-configure do
-  require 'redis'
-  ENV["REDISTOGO_URL"] = 'redis://rmantilla26:51144533@hollow-beach-577.heroku.com:6789'
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+$chat_data = {}
 
 run Sinatra::Application
--- secret-chat.rb
+++ secret-chat.rb
@@ -1,13 +1,91 @@
 require 'rubygems'
 require 'sinatra'
-require 'redis'
+
+def clear_data(chatID)
+  chat_room = $chat_data["#{chatID}"]
+  chat_room["messages"].each do |key,message|
+    if (Time.now.to_i - message["created_at"].to_i) > 15
+      $chat_data["#{chatID}"]["messages"].delete(key)
+    end
+  end unless chat_room["messages"].nil?
+end
+
+get '/' do
+  erb :home
+end
+
+get '/chat' do
+  chat_room = $chat_data["#{params["chatID"]}"]
+  @members = chat_room["members"]
+  @messages = chat_room["messages"]
+  @userID = params["userID"]
+  @chatID = params["chatID"]
+  erb :chat
+end
+
+get '/new_chat' do
+  params["userID"] = "#{rand(9999999)}#{Time.now.to_i}"
+  chat_room = {
+    "members" => {"#{params["userID"]}"=>{
+        "name"=>"#{params["userID"]}",
+        "updated_at" => "#{Time.now.to_i}"
+      }
+    },
+    "messages" => {"#{params["userID"]}#{Time.now.to_i}"=>{
+        "message" => "Welcome to #{params["chatID"]}",
+        "userID" => "#{params["userID"]}",
+        "created_at" => "#{Time.now.to_i}"
+      }
+    },
+    "password" => "",
+    "max_members" => "0",
+    "last_access" => "#{Time.now.to_i}"
+  }
+  $chat_data["#{params["chatID"]}"] = chat_room
+  redirect "/chat?chatID=#{params["chatID"]}&userID=#{params["userID"]}"
+end
+
+get '/enter_chat' do
+  params["userID"] = "#{rand(9999999)}#{Time.now.to_i}"
+  chat_room = $chat_data["#{params["chatID"]}"]
+  chat_room["members"]["#{params["userID"]}"]={
+    "name"=>"#{params["userID"]}",
+    "updated_at" => "#{Time.now.to_i}"
+  }
+  chat_room["last_access"] = "#{Time.now.to_i}"
+  
+  $chat_data["#{params["chatID"]}"] = chat_room
+  redirect "/chat?chatID=#{params["chatID"]}&userID=#{params["userID"]}"
+end
 
 get '/send_message' do
-  "Hello from Sinatra on Heroku!"
-  puts "REDIS EVIROMENT #{ENV.inspect}"
-#  uri = URI.parse(ENV["REDISTOGO_URL"] | "http://localhost:6379")
-#  puts "URI #{uri.inspect}"
-#  redis_client = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-#  puts "REDIS CLIENT :#{ENV["REDISTOGO_URL"]}"
-#  puts redis_client.inspect
+  chat_room = $chat_data["#{params["chatID"]}"]
+  
+  chat_room["members"]["#{params["userID"]}"]["updated_at"] = "#{Time.now.to_i}"
+  
+  chat_room["members"].each do |key,member|
+    if (Time.now.to_i - member["updated_at"].to_i) > 60
+     chat_room["members"].delete(key)
+    end
+  end unless chat_room["members"].nil?
+  
+  chat_room["messages"]["#{params["userID"]}#{Time.now.to_i}"]={
+    "message"=>"#{params["message"]}",
+    "userID"=>"#{params["userID"]}",
+    "created_at"=>"#{Time.now.to_i}"
+  }
+  chat_room["last_access"] = "#{Time.now.to_i}"
+  chat_room["messages"].each do |key,message|
+    if (Time.now.to_i - message["created_at"].to_i) > 15
+      chat_room["messages"].delete(key)
+    end
+  end unless chat_room["messages"].nil?
+  
+  $chat_data["#{params["chatID"]}"] = chat_room
+  redirect "/chat?chatID=#{params["chatID"]}&userID=#{params["userID"]}"
+end
+
+get '/get_chat' do
+  chat_room = $chat_data["#{params["chatID"]}"]
+  "CHAT INFO #{chat_room.inspect}"
 end
